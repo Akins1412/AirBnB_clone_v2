90204a18fd77a16855053975f389d2331bfa3957
Emmanuel <Akins1412>
2024-05-20 05:33:10 -0700

    initials

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""script that starts a Flask web application
+Your web application must be listening on 0.0.0.0
+"""
+
+from flask import Flask
+from flask import render_template
+from model import storage
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    "display a HTML page"
+    state = storage.all(State)
+    return  render_template("7-states_list.html", state=state)
+
+@app.teardown_appcontext
+def teardown(exempt):
+    "remove the current SQLAlchemy Session"
+    storage.close()
+
+
+
